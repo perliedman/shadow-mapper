2b7eaf5a2d4731bc4e585d7cde1d916db1e2d578
Per Liedman <per@liedman.net>
2014-06-25 12:29:42 +0200

    Requirements and naming

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ shadowmap = Extension('c_shadowmap',
     include_dirs=[numpy.get_include()],
     sources = ['shadowmap.c'])
 
-setup (name = 'Solsidan',
+setup (name = 'shadow-mapper',
        version = '0.0',
        description = 'Where\'s the sun?',
        ext_modules = [shadowmap])
